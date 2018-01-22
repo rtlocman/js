695c2c108a3eada4e541325fe3f1e95f99601343
RED6 <red6@ptc.local>
2018-01-22 10:49:44 +0200

    Hero services

--- heroesJs/src/app/app.module.ts
+++ heroesJs/src/app/app.module.ts
@@ -4,18 +4,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import {FormsModule} from "@angular/forms";
+import { FormsModule} from "@angular/forms";
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService} from "./hero.service";
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
+// import {@NgModule.providers
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule, FormsModule
   ],
-  providers: [],
+  providers: [ HeroService, MessageService ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule
+{
+
+}
